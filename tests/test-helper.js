ac0e1150f3a1ca1ae4cd2ea2d9f3a94a896453a4
Francis Paul Musango <francispaulmusango@gmail.com>
2024-08-13 22:31:29 +0300

    intial

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'railway-ember-starter/app';
+import config from 'railway-ember-starter/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
